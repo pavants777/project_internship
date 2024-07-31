dc2869fac31131140ea0966a420a7c57b9616135
Pavan Gowda T S <pavangowdats01@gmail.com>
2024-07-31 19:41:15 +0530

    last commit

--- backend/Routes/createQuiz.js
+++ backend/Routes/createQuiz.js
@@ -23,4 +23,24 @@ createRoutes.get('/quizzes', async (req, res) => {
     }
 });
 
+createRoutes.post('/joinquiz', async (req, res) => {
+    try {
+        const { joincode } = req.body;
+
+        if (!joincode) {
+            return res.status(400).json({ message: 'Join code is required' });
+        }
+
+        const quiz = await CreateQuiz.findOne({ joincode });
+
+        if (!quiz) {
+            return res.status(404).json({ message: 'Quiz not found' });
+        }
+
+        res.json(quiz);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
 module.exports = createRoutes;
